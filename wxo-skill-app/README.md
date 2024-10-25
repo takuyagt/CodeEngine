f506b2d54d79f12d0d3fc37864f35299e9ac143b
TAKUYA GOTO <tkyg@jp.ibm.com>
2024-10-25 19:32:45 +0900

    add wxo-skill-app

---
+++ wxo-skill-app/README.md
@@ -0,0 +1,24 @@
+# Example for wxo skill app
+
+## Requirements
+- Python 3.11
+
+## Setup Instructions
+
+### Deploy the wxo skill app to Code Engine
+
+1. [Create a project](https://cloud.ibm.com/docs/codeengine?topic=codeengine-manage-project#create-a-project) of Code Engine.
+2. [Deploy the application](https://cloud.ibm.com/docs/codeengine?topic=codeengine-app-source-code) from this repository source code.
+   - In **Create application**, click **Specify build details** and enter the following:
+      - Source
+         - Code repo URL: ex. `https://github.com/IBM/CodeEngine`
+         - Code repo access: `None`
+         - Branch name: `main`
+         - Context directory: `wxo-skill-app`
+      - Strategy
+         - Strategy: `Dockerfile`
+      - Output
+         - Enter your container image registry information.
+   - Open **Environment variables (optional)**, and add environment variables.
+   - We recommend setting **Min number of instances** to `1`.
+3. Confirm that the application status changes to **Ready**.
---
+++ wxo-skill-app/main.py
@@ -0,0 +1,62 @@
+from dateutil import parser
+import logging
+import os
+
+import flask
+
+# Enrichment task queue
+
+app = flask.Flask(__name__)
+app.logger.setLevel(logging.INFO)
+app.logger.handlers[0].setFormatter(
+    logging.Formatter(
+        "[%(asctime)s] %(levelname)s in %(module)s: %(message)s (%(filename)s:%(lineno)d)"
+    )
+)
+PORT = os.getenv("PORT", "8080")
+
+WEEKDAY = {
+    'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    'ja': ['月', '火', '水', '木', '金', '土', '日'],
+}
+
+
+@app.route("/weekday", methods=["POST"])
+def get_weekday():
+    """returns weekday
+    Request body:
+        datetime: (required) date time value in ISO format
+        language: (optional) language code (Supported: 'en', 'ja')
+
+    Returns:
+        body: weekday: weekday label
+        code: status code
+    """
+    data = flask.json.loads(flask.request.data)
+    app.logger.info(f"Received event: {data}")
+    sdt = data["datetime"]
+    dt = parser.isoparse(sdt)
+    lang = data.get("language", "en").lower()
+    if lang not in list(WEEKDAY.keys()):
+        return {"message": f"unsupported language: {lang}, supported languages: {list(WEEKDAY.keys())}"}
+    wd = WEEKDAY[lang][dt.weekday]       
+    return {"weekday": wd}, 200
+
+@app.rout("/range", methods=["POST"])
+def range_list():
+    """returns integer list
+    Request body:
+        size: (required) size of list
+
+    Returns:
+        body: weekday: weekday label
+        code: status code
+    """
+    data = flask.json.loads(flask.request.data)
+    app.logger.info(f"Received event: {data}")
+    s = int(data["size"])
+    return {"range": list(range(s))}, 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(PORT))
